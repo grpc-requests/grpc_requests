ab90dfaac57551c342df1f9c918eab0a1232dd38
Wayne Manselle <wayne@viridianforge.tech>
2024-05-27 13:08:06 -0700

    Sample add to script

--- tests.sh
+++ tests.sh
@@ -8,3 +8,8 @@ ruff check src/grpc_requests/*.py src/tests/*.py --statistics --config ruff.toml
 ruff format src/grpc_requests/*.py src/tests/*.py --check --config ruff.toml
 
 pytest --cov-report=xml --cov=src/grpc_requests
+
+# Wily Metrics - file by file for now
+wily report src/grpc_requests/client.py -n 1 difficulty effort complexity volume
+wily report src/grpc_requests/aio.py -n 1 difficulty effort complexity volume
+wily report src/grpc_requests/utils.py -n 1 difficulty effort complexity volume
