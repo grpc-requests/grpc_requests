d2ed7d6e71a26835cfb0178cb20d0ee52d0e6862
Wayne Manselle <wayne@viridianforge.tech>
2024-09-11 22:25:50 -0700

    Setup dependabot (#98)

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,6 @@
 import nox
 
+
 @nox.session
 @nox.parametrize(
     "python,protobuf",
@@ -12,6 +13,10 @@ import nox
 def test(session, protobuf):
     session.install("-e",".")
     session.install("-r", "requirements-test.txt")
+    # grpcio after version 1.65 is not compatible with protobuf 4.25.4
+    if protobuf == "4.25.4":
+        session.install("grpcio==1.65.5")
+        session.install("grpcio-reflection==1.65.5")
     session.install(f"protobuf=={protobuf}")
     session.run("pytest")
 
