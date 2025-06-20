f9510c79fb07f965d65e0cd508a9edc36c9101f9
Wayne Manselle <wayne@viridianforge.tech>
2025-06-19 23:03:42 -0700

    Update client_tester proto for expanding tests

--- src/tests/test_servers/client_tester/client_tester_pb2.pyi
+++ src/tests/test_servers/client_tester/client_tester_pb2.pyi
@@ -1,28 +1,36 @@
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class TestRequest(_message.Message):
-    __slots__ = ["factor", "readings", "uuid", "sample_flag", "request_name", "extra_data"]
+    __slots__ = ("factor", "readings", "uuid", "sample_flag", "request_name", "meta_data")
     FACTOR_FIELD_NUMBER: _ClassVar[int]
     READINGS_FIELD_NUMBER: _ClassVar[int]
     UUID_FIELD_NUMBER: _ClassVar[int]
     SAMPLE_FLAG_FIELD_NUMBER: _ClassVar[int]
     REQUEST_NAME_FIELD_NUMBER: _ClassVar[int]
-    EXTRA_DATA_FIELD_NUMBER: _ClassVar[int]
+    META_DATA_FIELD_NUMBER: _ClassVar[int]
     factor: int
     readings: _containers.RepeatedScalarFieldContainer[float]
     uuid: int
     sample_flag: bool
     request_name: str
-    extra_data: _containers.RepeatedScalarFieldContainer[bytes]
-    def __init__(self, factor: _Optional[int] = ..., readings: _Optional[_Iterable[float]] = ..., uuid: _Optional[int] = ..., sample_flag: bool = ..., request_name: _Optional[str] = ..., extra_data: _Optional[_Iterable[bytes]] = ...) -> None: ...
+    meta_data: _containers.RepeatedCompositeFieldContainer[Metadata]
+    def __init__(self, factor: _Optional[int] = ..., readings: _Optional[_Iterable[float]] = ..., uuid: _Optional[int] = ..., sample_flag: bool = ..., request_name: _Optional[str] = ..., meta_data: _Optional[_Iterable[_Union[Metadata, _Mapping]]] = ...) -> None: ...
+
+class Metadata(_message.Message):
+    __slots__ = ("key", "data")
+    KEY_FIELD_NUMBER: _ClassVar[int]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    key: str
+    data: bytes
+    def __init__(self, key: _Optional[str] = ..., data: _Optional[bytes] = ...) -> None: ...
 
 class TestResponse(_message.Message):
-    __slots__ = ["average", "feedback"]
+    __slots__ = ("average", "feedback")
     AVERAGE_FIELD_NUMBER: _ClassVar[int]
     FEEDBACK_FIELD_NUMBER: _ClassVar[int]
     average: float
